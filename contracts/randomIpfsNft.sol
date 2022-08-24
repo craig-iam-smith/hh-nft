5cb304252cec5893da70b7b9cb0792cdaf77f23b
Craig Smith <craig.iam.smith@gmail.com>
2022-08-24 12:30:06 -0600

    add tests and tags

--- contracts/randomIpfsNft.sol
+++ contracts/randomIpfsNft.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.7
+pragma solidity ^0.8.7;
 
 contract RandomIpfsNft {
 // when minting
-
+    uint256 _number;
 }
--- deploy/01-deploy-basic-nfts.js
+++ deploy/01-deploy-basic-nfts.js
@@ -23,3 +23,5 @@ module.exports = async function({getNamedAccounts, deployments}) {
     log ("---------------")
     
 }
+
+module.exports.tags = ["all", "basicnft"]
