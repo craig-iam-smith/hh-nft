4244544668c7ccf2a4c56429edcdb4abfb2e10b8
Craig Smith <craig.iam.smith@gmail.com>
2022-08-24 11:36:23 -0600

    initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+3 contracts
+
+1. Basic NFT
+2. Randomly created IPFS NFT
+3. Dyname SVG NFT
---
+++ deploy/01-deploy-basic-nfts.js
@@ -0,0 +1,25 @@
+const { deployMockContract } = require("ethereum-waffle")
+const { network, getNamedAccounts, deployments } = require("hardhat")
+const { developmentChains } = require("../helper-hardhat-config")
+const { verify } = require("../utils/verify")
+
+module.exports = async function({getNamedAccounts, deployments}) {
+    const {deploy, log} = deployments
+    const { deployer } = await getNamedAccounts()
+
+    log ("---------------------")
+    const args = []
+    log("basicNft")
+    const basicNft = await deploy("BasicNft", {
+        from: deployer,
+        args: args,
+        lo: true,
+        waitConfirmtions: network.config.blockConfirmations|| 1,
+    })
+    if (!developmentChains.includes(network.name) && process.env.ETHERSCAN_API_KEY) {
+        log("Verifying...")
+        await verify(basicNft.address, args)
+    }
+    log ("---------------")
+    
+}
